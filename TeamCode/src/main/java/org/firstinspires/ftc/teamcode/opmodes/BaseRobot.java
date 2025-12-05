57341a8c2b9a60f2eadca488669fa53e488e539e
Ben Beckwith <ben@codekaram.com>
2025-12-04 20:22:10 -0600

    refactor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BaseRobot.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class BaseRobot extends OpMode {
+
+    protected DcMotor flywheel;
+    protected DcMotor coreHex;
+    protected DcMotor leftDrive;
+    protected CRServo servo;
+    protected DcMotor rightDrive;
+
+
+    public static final int bankVelocity = 1300;
+    public static final int farVelocity = 1900;
+    public static final int maxVelocity = 2200;
+
+    @Override
+    public void init(){
+        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
+        coreHex = hardwareMap.get(DcMotor.class, "coreHex");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        servo = hardwareMap.get(CRServo.class, "servo");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+
+        // Establishing the direction and mode for the motors
+        flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel.setDirection(DcMotor.Direction.REVERSE);
+        coreHex.setDirection(DcMotor.Direction.REVERSE);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        //Ensures the servo is active and ready
+        servo.setPower(0);
+    }
+    @Override
+    public void loop(){
+        idle();
+    }
+    //Automatic Flywheel controls used in Auto and TeleOp
+
+    /**
+     * The bank shot or near velocity is intended for launching balls touching or a few inches from the goal.
+     * When running this function, the flywheel will spin up and the Core Hex will wait before balls can be fed.
+     * The servo will spin until the bumper is released.
+     */
+    public void BANK_SHOT_AUTO() {
+        ((DcMotorEx) flywheel).setVelocity(bankVelocity);
+        servo.setPower(1);
+        if (((DcMotorEx) flywheel).getVelocity() >= bankVelocity - 100) {
+            coreHex.setPower(1);
+        } else {    `
+            coreHex.setPower(0);
+        }
+    }
+
+    /**
+     * The far power velocity is intended for launching balls a few feet from the goal. It may require adjusting the deflector.
+     * When running this function, the flywheel will spin up and the Core Hex will wait before balls can be fed.
+     * The servo will spin until the bumper is released.
+     */
+    public void FAR_POWER_AUTO() {
+        ((DcMotorEx) flywheel).setVelocity(farVelocity);
+        servo.setPower(1);
+        if (((DcMotorEx) flywheel).getVelocity() >= farVelocity - 100) {
+            coreHex.setPower(1);
+        } else {
+            coreHex.setPower(0);
+        }
+    }
+
+    protected void idle(){
+        Thread.yield();
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ccAuto.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class ccAuto extends BaseRobot {
+
+    private ElapsedTime autoLaunchTimer = new ElapsedTime();
+    private ElapsedTime autoDriveTimer = new ElapsedTime();
+
+    private double WHEELS_INCHES_TO_TICKS = (28 * 5 * 3) / (3 * Math.PI);
+
+
+    @Override
+    protected void idle() {
+        super.idle();
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void loop() {
+        doAuto();
+    }
+//Autonomous Code
+//For autonomous, the robot will launch the pre-loaded 3 balls then back away from the goal, turn, and back up off the launch line.
+
+    /**
+     * For autonomous, the robot is using a timer and encoders on the drivetrain to move away from the target.
+     * This method contains the math to be used with the inputted distance for the encoders, resets the elapsed timer, and
+     * provides a check for it to run so long as the motors are busy and the timer has not run out.
+     */
+    private void autoDrive(double speed, int leftDistanceInch, int rightDistanceInch, int timeout_ms) {
+        autoDriveTimer.reset();
+        leftDrive.setTargetPosition((int) (leftDrive.getCurrentPosition() + leftDistanceInch * WHEELS_INCHES_TO_TICKS));
+        rightDrive.setTargetPosition((int) (rightDrive.getCurrentPosition() + rightDistanceInch * WHEELS_INCHES_TO_TICKS));
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftDrive.setPower(Math.abs(speed));
+        rightDrive.setPower(Math.abs(speed));
+        while ( (leftDrive.isBusy() || rightDrive.isBusy()) && autoDriveTimer.milliseconds() < timeout_ms) {
+            idle();
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    /**
+     * Blue Alliance Autonomous
+     * The robot will fire the pre-loaded balls until the 10 second timer ends.
+     * Then it will back away from the goal and off the launch line.
+     */
+    private void doAuto() {
+        telemetry.addData("RUNNING OPMODE", "Auto");
+        telemetry.update();
+        // Fire balls
+        autoLaunchTimer.reset();
+        while (autoLaunchTimer.milliseconds() < 10000) {
+            BANK_SHOT_AUTO();
+            telemetry.addData("Launcher Countdown", autoLaunchTimer.seconds());
+            telemetry.update();
+        }
+        ((DcMotorEx) flywheel).setVelocity(0);
+        coreHex.setPower(0);
+        servo.setPower(0);
+        // Back Up
+        autoDrive(0.5, -12, -12, 5000);
+        // Turn
+        autoDrive(0.5, -8, 8, 5000);
+        // Drive off Line
+        autoDrive(1, -50, -50, 5000);
+    }
+}
